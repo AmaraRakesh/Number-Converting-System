720c722022d6d15ec7b05af5c2edff5c9de30767
AmaraRakesh <154523716+AmaraRakesh@users.noreply.github.com>
2024-03-24 15:19:24 +0530

    Add files via upload

---
+++ conversion.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+void oct_number(int);
+void hexa_number(int);
+void binary_number(int);
+int octal_deci_number(int );
+int hexa_deci_number(char *);
+int binary_number1(int );
+void welcome_stuff(void);
+void conversion_code(void);
+#define Line printf("\n");
+#define clear system("clear");
